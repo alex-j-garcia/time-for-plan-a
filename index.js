f30bb54acb26834515e99f8d492236cd7de0cf9f
Alex J Garcia <alexshuu@gmail.com>
2024-02-10 15:06:04 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Import the functions you need from the SDKs you need
+var app_1 = require("firebase/app");
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+};
+// Initialize Firebase
+var app = (0, app_1.initializeApp)(firebaseConfig);
